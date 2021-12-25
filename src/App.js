13b1c93b48ed26359f1871cc7380dae28fae7513
Daniel <d.pesevski92@gmail.com>
2021-12-25 18:13:17 +0100

    clean api call with react hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect } from 'react';
+
+import useApi from './useApi';
+
+import commentsApi from './api/comments';
+import postsApi from './api/posts';
 
 function App() {
+  const getPostsApi = useApi(postsApi.getPosts);
+  const getCommentsApi = useApi(commentsApi.getComments);
+  
+  useEffect(() => {
+    getPostsApi.request();
+    getCommentsApi.request();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Post List */}
+      <div>
+        <h1>Posts</h1>
+        {getPostsApi.loading && <p>Posts are loading...</p>}
+        {getPostsApi.error && <p>{getPostsApi.error}</p>}
+        <ul>
+          {getPostsApi.data?.map((post) => (
+            <li key={post.id}>{post.title}</li>
+          ))}
+        </ul>
+      </div>
+
+      {/* Comment List */}
+      <div>
+        <h1>Comments</h1>
+        {getCommentsApi.loading && <p>Comments are loading...</p>}
+        {getCommentsApi.error && <p>{getCommentsApi.error}</p>}
+        <ul>
+          {getCommentsApi.data?.map((comment) => (
+            <li key={comment.id}>{comment.name}</li>
+          ))}
+        </ul>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
